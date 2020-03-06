01d7d1f408ec0537993edc147e595b903c31c1f9
Ganobis <kasiaodi@gmail.com>
2020-03-06 20:08:35 +0100

    Add DotNet
    new folder dotnet with first ex, and some second

---
+++ DotNet/lista 1/zad1/zad1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace zad1
+{
+    class Program
+    {
+        private static bool prosta_funkcja(int liczba)
+        {
+            int liczba1 = liczba;
+            int suma = 0;
+            while (liczba1 != 0)
+            {
+                int curr = liczba1 % 10;
+                liczba1 /= 10;
+                suma += curr;
+                if ((curr != 0) && (liczba%curr != 0))
+                {
+                    return false;
+                }
+            }
+            if ((suma != 0) && (liczba%suma == 0))
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            for (int i=0;i<100000;i++)
+            {
+                if (prosta_funkcja(i))
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ DotNet/lista 1/zad2/zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Grid
+    {
+
+    }
+}
