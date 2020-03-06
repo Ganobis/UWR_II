c1af216943c07ebaaebda8fdf377f0ad44143151
Ganobis <kasiaodi@gmail.com>
2020-03-06 21:05:52 +0100

    change 2ex
    not complet 2ex

--- DotNet/lista 1/zad2/zad2/Program.cs
+++ DotNet/lista 1/zad2/zad2/Program.cs
@@ -2,16 +2,20 @@
 
 namespace zad2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Grid grid = new Grid(4, 4);
+            int rowdata = grid.tab[1,2];
+
             Console.WriteLine("Hello World!");
         }
     }
 
-    class Grid
+    public class Grid
     {
-
+        public int[,] tab { get; set; }
+        public Grid(int x, int y) => tab = new int[x, y];
     }
 }
