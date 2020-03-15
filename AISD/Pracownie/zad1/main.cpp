eaea376f35fdbfbdcc515f67a09c7d30e16edc7c
Ganobis <kasiaodi@gmail.com>
2020-03-15 16:48:35 +0100

    edit aist ex1

--- AISD/Pracownie/zad1/main.cpp
+++ AISD/Pracownie/zad1/main.cpp
@@ -2,12 +2,15 @@
 #include <vector>
 
 bool is_sidewalk(int n, short data[][3]){
-    std::vector<bool> is_use;
-    if(n > 10000)
-        is_use.resize(10000);
-    else
-        is_use.resize(n);
-    std::cout<<is_use[0]<<is_use[450]<<is_use[9999];
+    bool is_use[10000];
+    std::vector<short[3]> end_bridge[10000];
+    short temp[3];
+    for (int i = 0; i < n ; i++) {
+        temp[0]=0;
+        temp[1]=data[i][1];
+        temp[2]=data[i][2];
+        end_bridge[data[i][0]].push_back(temp);
+    }
     return false;
 }
 
@@ -15,8 +18,8 @@ int main(){
     int n;
     std::cin>>n;
     short data[n][3];
-    //for (int i = 0; i < n; ++i)
-    //    std::cin>>data[i][0]>>data[i][1]>>data[i][2];
+    for (int i = 0; i < n; ++i)
+        std::cin>>data[i][0]>>data[i][1]>>data[i][2];
     if(!is_sidewalk(n, data))
         std::cout<<"BRAK";
     return 0;
