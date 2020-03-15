4fa87d6e7098116adf699f99c7dd58d6520a5bce
Wojciech <kasiaodi@gmail.com>
2020-03-15 20:52:44 +0100

    close to the solution ex1 AISD

--- AISD/Pracownie/zad1/main.cpp
+++ AISD/Pracownie/zad1/main.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
 #include <vector>
 
+short bricks[10001];
+short counter;
+
+bool find_x(bool is_use[10001], std::vector<int> v[10001], short wanted){
+    if (wanted == 0 && counter != 0)
+        return true;
+    else{
+        counter++;
+        if (!is_use[wanted])
+            return false;
+        else{
+            is_use[wanted] = false;
+            for (int i = 0; i < v[wanted].size(); ++i)
+            {
+                if (find_x(is_use, v, v[wanted][i] >> 16))
+                {
+                    counter--;
+                    bricks[counter] = v[wanted][i];
+                    if(bricks[counter] == 0)
+                        bricks[counter] = -1;
+                    return true;
+                }
+                else
+                    counter--;
+            }
+        }
+    }
+}
+
 bool is_sidewalk(int n, short data[][3]){
-    bool is_use[10000];
-    std::vector<short[3]> end_bridge[10000];
-    short temp[3];
+    bool is_use[10001];
+    std::vector<int> end_bridge[10001];
+    int temp;
     for (int i = 0; i < n ; i++) {
-        temp[0]=0;
-        temp[1]=data[i][1];
-        temp[2]=data[i][2];
+        is_use[data[i][0]] = true;
+        temp = data[i][2];
+        temp <<= 16;
+        temp += i;
         end_bridge[data[i][0]].push_back(temp);
     }
+    if(find_x(is_use, end_bridge, 0))
+        return true;
     return false;
 }
+
 
 int main(){
     int n;
@@ -20,8 +53,20 @@ int main(){
     short data[n][3];
     for (int i = 0; i < n; ++i)
         std::cin>>data[i][0]>>data[i][1]>>data[i][2];
-    if(!is_sidewalk(n, data))
+    if(!is_sidewalk(n, data)){
         std::cout<<"BRAK";
+        return 0;
+    }
+    n = 0;
+    while(bricks[n] != 0)
+        n++;
+    std::cout << n << std::endl;
+    for (int i = 0; i < n; ++i)
+    {
+        if (bricks[i] == -1)
+            std::cout << data[0][0] << " " << data[0][1] << " " << data[0][2] << std::endl; 
+        else
+            std::cout << data[bricks[i]][0] << " " << data[bricks[i]][1] << " " << data[bricks[i]][2] << std::endl;
+    }
     return 0;
 }
-
---
+++ AISD/Pracownie/zad1/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+short idd[4];
+
+int main(int argc, char const *argv[])
+{
+	short i;
+	int x;
+	x = 32;
+	x <<= 16;
+	x += 42;
+	std::vector<int> vector1[4];
+	vector1[0].push_back(1);
+	vector1[0].push_back(1);
+	vector1[3].push_back(0);
+	std::cout << vector1[0].size() << " " << vector1[1].size() << " " << vector1[3].size() << std::endl;
+	std::cout << idd[0] << std::endl;
+	i = x;
+	std::cout << i << " " << x;
+
+	return 0;
+}
