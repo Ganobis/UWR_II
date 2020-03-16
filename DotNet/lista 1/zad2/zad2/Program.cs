5e0c95c98c98944498d7a5cb5e1a4acc69b1f446
Ganobis <kasiaodi@gmail.com>
2020-03-16 12:29:51 +0100

    1.1-3 .net done

--- DotNet/lista 1/zad2/zad2/Program.cs
+++ DotNet/lista 1/zad2/zad2/Program.cs
@@ -2,20 +2,61 @@
 
 namespace zad2
 {
-    public class Program
+    public class Grid
     {
-        static void Main(string[] args)
-        {
-            Grid grid = new Grid(4, 4);
-            int rowdata = grid.tab[1,2];
+        public int x, y; 
+        public int[,] tab;
+        public Grid(int x, int y) { tab = new int[x, y]; this.x = x; this.y = y; }
 
-            Console.WriteLine("Hello World!");
+
+        public int[] this[int k]
+        {
+            get
+            {
+                int[] values = new int[x];
+                for (int i = 0; i < x; i++)
+                    values[i] = this.tab[k, i];
+
+                return values;
+            }
+        }
+
+
+
+    public int this[int i, int j]
+        {
+            get => tab[i, j];
+            set => tab[i, j] = value;
         }
     }
 
-    public class Grid
+
+    class Program
     {
-        public int[,] tab { get; set; }
-        public Grid(int x, int y) => tab = new int[x, y];
+        static void Main()
+        {
+            Grid grid1 = new Grid(3, 3);
+            int a = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    grid1[i, j] = a;
+                    a++;
+                }
+            }
+            Console.WriteLine(grid1[1][1]);
+
+            int[] rowdata = grid1[1];
+
+            foreach (var item in rowdata)
+                Console.Write(item.ToString() + " ");
+
+            Console.WriteLine();
+           
+            foreach (var item in grid1.tab)
+                Console.Write(item.ToString() + " ");
+
+        }
     }
 }
--- DotNet/lista 1/zad3/zad3/zad3/Program.cs
+++ DotNet/lista 1/zad3/zad3/zad3/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Reflection;
 
 namespace zad3
 {
+    class Example
+    {
+        private void PrivateMethod()
+        {
+            Console.WriteLine("This is a private method!");
+        }
+        public void PublicMetod()
+        {
+            Console.WriteLine("This is a public method#");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime Start, Stop;
+
+            Start = DateTime.Now;
+            typeof(Example).GetMethod("PrivateMethod", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(new Example(), null);
+            Stop = DateTime.Now;
+
+            TimeSpan Czas = Stop - Start;
+            Console.WriteLine("Czas dla metody prywatniej: " + Czas);
+
+            Start = DateTime.Now;
+            Example ex = new Example();
+            ex.PublicMetod();
+            Stop = DateTime.Now;
+
+            Czas = Stop - Start;
+            Console.WriteLine("Czas dla metody publicznej: " + Czas);
         }
     }
 }
