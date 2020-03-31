d1cf5a45deb1818d43af753bb6ff1f1bab725812
Ganobis <kasiaodi@gmail.com>
2020-03-31 12:52:06 +0200

    Rpis list 4

--- DotNet/lista 2/zad2/zad2/Program.cs
+++ DotNet/lista 2/zad2/zad2/Program.cs
@@ -2,11 +2,80 @@
 
 namespace zad2
 {
-   class BinaryTreeNode<T>
+    class BinaryTreeNode<T>
     {
-        private class Node
+        private class Node<T>
         {
+            public Node<T> left;
+            public Node<T> right;
+            public T data;
+            
+            public Node(T data)
+            {
+                left = null;
+                right = null;
+                this.data = data;
+            }
 
+            public String Write_Tree()
+            {
+                String left = "";
+                if (this.left != null) left = this.left.Write_Tree();
+
+                String right = "";
+                if (this.right != null) right = this.right.Write_Tree();
+
+                return left + " " + data + " " + right;
+
+            }
+        }
+
+        private Node<T> root;
+
+        public void insert(T data)
+        {
+            if(root == null)
+            {
+                root = new Node<T>(data);
+            }
+            else
+            {
+                Node<T> son = root;
+
+                while (son != null)
+                {
+                    if (data.CompareTo(son.data) > 0)
+                    {
+                        if (son.right == null)
+                            break;
+                        son = son.right;
+                    }
+                    else
+                    {
+                        if (son.left == null)
+                            break;
+                        son = son.left;
+                    }
+                }
+                Node<T> n = new Node<T>(data);
+                if ((son.data).CompareTo(data) > 0)
+                {
+                    son.right = n;
+                }
+                else
+                {
+                    son.left = n;
+                }
+            }
+        }
+
+        public String Write_Tree()
+        {
+            if (root != null)
+            {
+                return root.Write_Tree();
+            }
+            else return "EMPTY";
         }
     }
     class Program
