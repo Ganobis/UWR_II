333416c8a7a1ed323ed988cc38b417b847641df0
Ganobis <kasiaodi@gmail.com>
2020-03-31 22:21:09 +0200

    add .net 1.2.3

---
+++ DotNet/lista 2/zad3/zad3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zad3
+{
+    class Program
+    {
+        private static void write_list(List<int> l)
+        {
+            Console.WriteLine("Your list:");
+            foreach (int i in l)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        private static bool find_div20(int x)
+        {
+            if (x % 20 == 0)
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>();
+            for(int i = 0; i < 50; i++)
+            {
+                list1.Add(i);
+            }
+
+            write_list(list1);
+
+            List<int> list2 = list1.ConvertAll(x => 4 * x);
+            write_list(list2);
+
+            List<int> list3 = list2.FindAll(find_div20);
+            write_list(list3);
+
+            Console.WriteLine($"Remove {list1.RemoveAll(match: find_div20)} elements form list1");
+            write_list(list1);
+
+            List<int> list4 = new List<int>();
+            list4.Add(34);
+            list4.Add(12);
+            list4.Add(99);
+            list4.Add(122);
+            list4.Add(756);
+            list4.Add(-34);
+            write_list(list4);
+            list4.Sort();
+            write_list(list4);
+        }
+    }
+}
