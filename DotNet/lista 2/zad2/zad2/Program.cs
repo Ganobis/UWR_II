2679411c1ecae7748c2c842ce4b3009381e6b810
Ganobis <kasiaodi@gmail.com>
2020-04-01 01:02:43 +0200

    dotnet ex1.2.2

--- DotNet/lista 2/zad2/zad2/Program.cs
+++ DotNet/lista 2/zad2/zad2/Program.cs
@@ -20,13 +20,71 @@ namespace zad2
             public String Write_Tree()
             {
                 String left = "";
-                if (this.left != null) left = this.left.Write_Tree();
+                if (this.left != null) 
+                    left = this.left.Write_Tree();
 
                 String right = "";
-                if (this.right != null) right = this.right.Write_Tree();
+                if (this.right != null) 
+                    right = this.right.Write_Tree();
 
-                return left + " " + data + " " + right;
+                return "(" + left + " " + data + " " + right + ")";
 
+            }
+
+            public String DFS_without()
+            {
+                String left = "";
+                if (this.left != null)
+                    left = this.left.DFS_without();
+
+                String right = "";
+                if (this.right != null)
+                    right = this.right.DFS_without();
+
+                return data + " " + left + " " + right;
+            }
+
+            private int depth()
+            {
+                int d_left = 0, d_right = 0;
+
+                if (left != null)
+                    d_left = 1 + left.depth();
+
+                if (right != null)
+                    d_right = 1 + right.depth(); 
+
+                if (d_left > d_right)
+                    return d_left;
+                else return d_right;
+            }
+
+            private String show_level(int x)
+            {
+                String ret = "";
+                if (x == 0)
+                    ret += data;
+                else
+                {
+                    String l = "";
+                    String r = "";
+                    if(left != null)
+                        l = left.show_level(x - 1);
+                    if (right != null)
+                        r = right.show_level(x - 1);
+                    ret = l + " " + r;
+                }
+                return ret;
+            }
+            public String BFS_without()
+            {
+                String ret = "";
+                ret += data;
+                for(int i = 1; i < depth() + 1; i++)
+                {
+                    ret += " " + show_level(i);
+                }
+                return ret;
             }
         }
 
@@ -44,7 +102,7 @@ namespace zad2
 
                 while (son != null)
                 {
-                    if (data.CompareTo(son.data) > 0)
+                    if(Convert.ToInt32(data).CompareTo(Convert.ToInt32(son.data)) > 0)
                     {
                         if (son.right == null)
                             break;
@@ -58,7 +116,7 @@ namespace zad2
                     }
                 }
                 Node<T> n = new Node<T>(data);
-                if ((son.data).CompareTo(data) > 0)
+                if (Convert.ToInt32(data).CompareTo(Convert.ToInt32(son.data)) > 0)
                 {
                     son.right = n;
                 }
@@ -75,14 +133,52 @@ namespace zad2
             {
                 return root.Write_Tree();
             }
-            else return "EMPTY";
+            else return "EMPTY!";
+        }
+
+        public String DFS_without()
+        {
+            if (root != null)
+            {
+                return root.DFS_without();
+            }
+            else return "EMPTY!";
+        }
+
+        public String BFS_without()
+        {
+            if (root != null)
+            {
+                return root.BFS_without();
+            }
+            else return "EMPTY!";
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinaryTreeNode<int> tree = new BinaryTreeNode<int>();
+            Console.WriteLine(tree.Write_Tree());
+            tree.insert(5);
+            tree.insert(3);
+            tree.insert(4);
+            tree.insert(9);
+            tree.insert(1);
+            tree.insert(0);
+            tree.insert(2);
+            Console.WriteLine(tree.Write_Tree());
+            Console.WriteLine(tree.DFS_without());
+            Console.WriteLine(tree.BFS_without());
+
+            BinaryTreeNode<String> tree2 = new BinaryTreeNode<String>();
+            tree2.insert("222");
+            tree2.insert("111");
+            tree2.insert("333");
+            tree2.insert("000");
+            Console.WriteLine(tree2.Write_Tree());
+            Console.WriteLine(tree2.DFS_without());
+            Console.WriteLine(tree2.BFS_without());
         }
     }
 }
