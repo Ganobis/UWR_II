25e522edaebd7778f0829e5dd46c593e036deccf
Ganobis <kasiaodi@gmail.com>
2020-06-02 21:00:00 +0200

    commint dotnet 9.5 and 9.6

--- DotNet/lista 9/zad5/Program.cs
+++ DotNet/lista 9/zad5/Program.cs
@@ -1,12 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml;
 
-namespace zad5
+class zad5
 {
     class Program
     {
+        public class Student
+        {
+            public string Imie;
+            public string Nazwisko;
+            public string Data;
+            public string Adres_Staly;
+            public string Adres_Tymczasowy;
+            public Zajecia Zajecia;
+            public Student(string i, string n, string d, string staly, string tymcz, Zajecia z)
+            {
+                Imie = i;
+                Nazwisko = n;
+                Data = d;
+                Adres_Staly = staly;
+                Adres_Tymczasowy = tymcz;
+                Zajecia = z;
+            }
+            public override string ToString()
+            {
+                string res = "Imie: " + Imie  
+                    + "\nNazwiasko: " + Nazwisko
+                    + "\nData urodzenia: " + Data
+                    + "\nAdres staly: " + Adres_Staly
+                    + "\nAdres symczasowy: "  + Adres_Tymczasowy
+                    + "\nPrzedmioty i ocena: \n\t";
+
+                foreach (var elem in Zajecia.PrzedmiotOcena)
+                {
+                    res += "" + elem.Key + " -- " + elem.Value + "\n\t";
+                }
+
+                return res;
+            }
+        }
+        public class Zajecia
+        {
+            public Zajecia() { PrzedmiotOcena = new Dictionary<string, string>(); }
+            public Dictionary<string, string> PrzedmiotOcena;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                List<Student> studenci = new List<Student>();
+                XmlDocument document = new XmlDocument();
+                document.Load("dane.xml");
+
+                foreach (XmlNode node in document.DocumentElement)
+                {
+                    string imie = node["Imię"].InnerXml;
+                    string nazwisko = node["Nazwisko"].InnerXml;
+                    string data = node["DataUr"].InnerXml;
+                    string AdresStaly = node["AdresStały"].InnerXml;
+                    string AdresTymczasowy = node["AdresTym"].InnerXml;
+                    Zajecia lista = new Zajecia();
+
+                    XmlNode dziecko = node["ListaZajęć"];
+                    foreach (XmlNode inner in dziecko)
+                    {
+                        lista.PrzedmiotOcena.Add(inner["Nazwa"].InnerXml, inner["Ocena"].InnerXml);
+                    }
+
+                    Student nowy = new Student(imie, nazwisko, data, AdresStaly, AdresTymczasowy, lista);
+                    studenci.Add(nowy);
+                }
+                foreach (var student in studenci)
+                    Console.WriteLine(student.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ DotNet/lista 9/zad6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+
+namespace zad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = null;
+
+            try
+            {
+                reader = new XmlTextReader("dane.xml");
+                reader.WhitespaceHandling = WhitespaceHandling.None;
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Text)
+                        Console.Write(reader.Value + "\t");
+                    else if (reader.NodeType == XmlNodeType.Element)
+                        Console.WriteLine();
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+    }
+}
