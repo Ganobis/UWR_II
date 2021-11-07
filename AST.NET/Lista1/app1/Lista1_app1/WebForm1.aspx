82b29c99e7b3e2078225f26ba56f8f7238d727a9
Ganobis <kasiaodi@gmail.com>
2021-11-07 15:27:18 +0100

    commit

---
+++ AST.NET/Lista1/app1/Lista1_app1/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Lista1_app1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label id="Label2" runat="server" />
+            <asp:Button id="Button1" text="Zaczni liczyć" runat="server" OnClick="Button1_Click" />
+            <br/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AST.NET/Lista1/app1/Lista1_app1/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lista1_app1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            while (true)
+            {
+                x++;
+                Label2.Text = "Czas " + x;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
